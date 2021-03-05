e5cf266612288213c7b1fdb9a671612a503c2a93
unknown <popovstanislav@abv.bg>
2021-03-05 12:23:15 +0200

    some backend changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,22 +2,43 @@ const express = require('express');
 const router = express.Router();
 let userModel = require('../models/signUpModel')
 
+router.post('/register',async (req,res)=>{
+    try {
+        let errors = []
+        const {email,password,username} = req.body;
+        if(email.length == 0 || email.length < 6) {
+            errors.push('Email must be more than 5 simbols');
+        }
+        if(username.length === 0 || username.length < 6) {
+            errors.push('Username must be more than 5 simbols');
+        }
+        if(password.length === 0 || password.length < 6) {
+            errors.push('Password must be more than 5 simbols');
 
-router.route('/').get((req,res)=>{
-    userModel.find({})
-    .then(users=>res.json(users))
-    .catch(err=>res.status(400).json(err))
+        }
+        if(errors.length > 0) {
+          return res.status(401).json({error: errors.join(',')});
+        }
+
+       await userModel.create({username,email,password});
+       res.send(`user created successfully`)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-router.route('/add').post((req,res)=>{
-    const {username,email,password} = req.body;
-    userModel.create({username,email,password})
-    .then(response=>res.json('User added'))
-    .catch(err=>console.log(err))
 
-    
+
+router.get('/users',async(req,res)=>{
+    try {
+        const {username,email}=req.body;
+     let data =   await userModel.find({});
+        res.send(data)
+
+    } catch (error) {
+        console.log(error)
+    }
 })
-
 
 
 module.exports = router
